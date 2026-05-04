863f12b53275cbaced9fcc0882e44e5988c32ccc
radek-or <radek.or@gmail.com>
2026-05-04 17:52:48 +0200

    Update Playwright configuration to set baseURL and adjust reporter settings for better test management.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
-    trace: 'on-first-retry',
+    baseURL: 'https://localhost:3000',
+    trace: 'on',
   },
 
   projects: [
